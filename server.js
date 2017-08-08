99c88d4a7f6a3ebcaa37d2cc67c3490b3937f61e
T76-DC <k98apps@gmail.com>
2017-08-08 20:29:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/articleOne',function(req,res){
     res.send("This is one article page");
-})
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
